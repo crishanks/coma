e6ec00488590dfc93e4075b0c5f812895b730540
github username <crishanks12@gmail.com>
2019-03-25 12:04:41 -0700

    updated main nav bar, added resources sub bar, added start game card, added event listener for start game button click

--- notes_frontend/src/index.js
+++ notes_frontend/src/index.js
@@ -1,2 +1,16 @@
+const startDiv = document.getElementById('start-div');
+const startButton = document.getElementById('start-button');
+startButton.addEventListener('click', () => {
+  startDiv.remove();
+  startGame();
+});
 
+function startGame() {
+  //Show resources nav bar with pre-set values
+  // deals first hand of face-down cards w/ click listeners
+  //
 
+  const resourcesBar = document.getElementById('resources-bar');
+  resourcesBar.style.visibility = "visible";
+
+}
